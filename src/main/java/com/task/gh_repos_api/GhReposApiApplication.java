61b0fcfe44e778f18dc66756ab6adc012d47ec26
bartoszkocylo <bartoszk236@gmail.com>
2025-08-11 13:17:09 +0200

    project initialization

---
+++ src/main/java/com/task/gh_repos_api/GhReposApiApplication.java
@@ -0,0 +1,13 @@
+package com.task.gh_repos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhReposApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhReposApiApplication.class, args);
+	}
+
+}
